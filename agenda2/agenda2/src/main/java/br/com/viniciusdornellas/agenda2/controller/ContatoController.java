b64b0a86bd860f366347429d547071c677c1b5d4
viniciusdornellas <105527357+viniciusdornellas@users.noreply.github.com>
2022-12-13 21:50:56 -0300

    controller

--- agenda2/agenda2/src/main/java/br/com/viniciusdornellas/agenda2/controller/ContatoController.java
+++ agenda2/agenda2/src/main/java/br/com/viniciusdornellas/agenda2/controller/ContatoController.java
@@ -2,10 +2,13 @@ package br.com.viniciusdornellas.agenda2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
 
 import br.com.viniciusdornellas.agenda2.dto.ContatoDTO;
 import br.com.viniciusdornellas.agenda2.dto.ContatoResponseDTO;
@@ -18,15 +21,15 @@ public class ContatoController {
 	private final IContatoService service;
 	
 	@Autowired
-	public ContatoController(@Qualifier("ContatoServiceAWSImpl") IContatoService service) {
+	public ContatoController(@Qualifier("ContatoServiceImpl") IContatoService service) {
 		this.service = service;
 	}
 
 	@PostMapping("/cadastrar")
-	public ContatoResponseDTO cadastrar (@RequestBody ContatoDTO request) {
+	public ResponseEntity<ContatoResponseDTO> cadastrar (@RequestBody ContatoDTO request) {
 		
 		service.salvar(request);
-		return null;
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 	
 }
--- agenda2/agenda2/src/main/java/br/com/viniciusdornellas/agenda2/service/impl/ContatoServiceImpl.java
+++ agenda2/agenda2/src/main/java/br/com/viniciusdornellas/agenda2/service/impl/ContatoServiceImpl.java
@@ -1,5 +1,9 @@
 package br.com.viniciusdornellas.agenda2.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +11,24 @@ import br.com.viniciusdornellas.agenda2.dto.ContatoDTO;
 import br.com.viniciusdornellas.agenda2.service.IContatoService;
 
 @Service
+@Qualifier("ContatoServiceImpl")
 public class ContatoServiceImpl implements IContatoService{
+
+	
+	private List<ContatoDTO> contatos;
+	
+	
+	
+	public ContatoServiceImpl() {
+		this.contatos = new ArrayList<ContatoDTO>();
+	}
 
 	@Override
 	public void salvar(ContatoDTO contato) {
 		// TODO Auto-generated method stub
 		System.out.println("Cadastrado com sucesso!");
+		this.contatos.add(contato);
+		System.out.println(contatos);
 	}
 
 }
